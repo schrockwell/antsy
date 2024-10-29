d6f0b2d9e26279db30c7dac60169c7a6baf3aedb
Rockwell Schrock <rockwell@schrock.me>
2024-10-29 09:12:45 -0400

    Fix mix_test_watch dep

--- mix.exs
+++ mix.exs
@@ -35,10 +35,8 @@ defmodule Antsy.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:mix_test_watch, "~> 1.2.0"},
+      {:mix_test_watch, "~> 1.2.0", only: [:dev, :test], runtime: false},
       {:ex_doc, "~> 0.34.2", only: :dev, runtime: false}
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
     ]
   end
 end
