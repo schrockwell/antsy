c085a56733f0aeab65b001690d98cf47dfd657d8
Rockwell Schrock <rockwell@schrock.me>
2024-10-29 09:13:13 -0400

    v0.2.1

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Antsy.MixProject do
   def project do
     [
       app: :antsy,
-      version: "0.2.0",
+      version: "0.2.1",
       elixir: "~> 1.16",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
